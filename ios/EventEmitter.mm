39fcc204b4fc2ad79c2d926a6ede82015a08ba87
Bohdan Hasyn <bodich163@gmail.com>
2024-05-22 18:35:57 +0300

    fix: testing

--- ios/EventEmitter.mm
+++
@@ -1,8 +0,0 @@
-#import "React/RCTBridgeModule.h"
-
-@interface RCT_EXTERN_MODULE(EventEmitter, NSObject)
-
-RCT_EXTERN_METHOD(on:(NSString *)event listener:(RCTResponseSenderBlock)callback)
-RCT_EXTERN_METHOD(emit:(NSString *)event arg:(id)arg)
-
-@end
